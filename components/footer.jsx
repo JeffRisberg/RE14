f59e9c16785d6b40751407a7934082da4ced4a4f
JeffRisberg <jrisberg@aol.com>
2017-02-28 00:02:56 +0000

    updated versions

--- components/footer.jsx
+++ components/footer.jsx
@@ -7,7 +7,7 @@ module.exports = Footer = React.createClass({
         <hr />
         <div className="row-fluid">
           <div className="span12">
-            <div>React Quickly by Azat (<a href="http://twitter.com/azat_co" target="_blank">@azat_co</a>)</div>
+            <div>The Risberg Family</div>
           </div>
         </div>
       </div>
